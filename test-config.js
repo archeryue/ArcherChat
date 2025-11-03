d71b6d00dde3743f0adb2bf5abfc99f80e436155
archeryue <archeryue7@gmail.com>
2025-11-03 13:11:01 -0800

    Test: Modify file to trigger Gitleaks scan in THIS commit

--- test-config.js
+++ test-config.js
@@ -1,9 +1,11 @@
 // TEST FILE: This should trigger Gitleaks secret scanning
 // This is a fake API key for testing purposes
+// Updated to trigger a new commit
 
 const testConfig = {
-  // This fake Google API key should be caught by Gitleaks
-  apiKey: "AIzaSyDFakeKeyForTestingPurposes123456"
+  // This fake Google API key should be caught by Gitleaks NOW
+  apiKey: "AIzaSyDFakeKeyForTestingPurposes123456",
+  timestamp: Date.now()
 };
 
 module.exports = testConfig;
